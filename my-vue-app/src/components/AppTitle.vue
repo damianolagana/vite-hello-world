e4b0d82d7e2e0600397e1bbfe3c1686ada896098
damianolagana <damianolagana@gmail.com>
2023-05-18 17:24:26 +0200

    AppTitle

---
+++ my-vue-app/src/components/AppTitle.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: "AppTitle",
+}
+
+</script>
+
+<template>
+    <h1>La mia prima app con <span class="blue">Vite</span></h1>
+</template>
+
+<style scoped>
+.blue {
+    color: blue;
+}
+</style>
